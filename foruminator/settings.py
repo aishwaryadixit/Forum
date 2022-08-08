c350e8b8b5f1be75673628f370774592d384718d
Aishwarya Dixit <aishwarya.dixit1995@gmail.com>
2022-08-08 16:28:46 +0000

    `ALLOWED_HOSTS`: add `localhost` for containerised development

--- foruminator/settings.py
+++ foruminator/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-@vfxo^r3j4-0!dlofd0bb6m4ryl-bseig33el+7m%2a#mabrg2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost"]
 
 
 # Application definition
